7d091c82dfb5352e2ccd910335b8fb6a4ee5881a
Jessica <jssclgh@gmail.com>
2024-02-01 18:12:17 -0600

    added a README for the project

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown.js")
@@ -70,7 +70,7 @@ const questions =
 
 
 
-// TODO: Create a function to initialize app
+// This function initializes the app
 function init() {
     inquirer
         .prompt(questions)
